e6fcb72acb17b2329d659565a8219b31d22f1c0e
Aneirine <anyashi2002@gmail.com>
2021-03-21 23:34:21 +0200

    Add payer and payment  entities

---
+++ src/main/java/com/aneirine/service/entities/Payment.java
@@ -0,0 +1,25 @@
+package com.aneirine.service.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "payments")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String cart;
+
+}
